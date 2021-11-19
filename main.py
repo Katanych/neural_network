e97288ed427220e5c585e25d52259f9bca6fcabb
Katanych <k4t4ny4@mail.ru>
2021-11-19 10:45:47 +0700

    create first example

---
+++ main.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+WEIGHTS = np.array([0.1, 0.2, 0])
+
+
+def neural_network(inp, weights):
+    """Функция нейронной сети.
+
+    :param inp: вектор, в котором первый элемент - текущее число игр, второй - процент побед, третий - болельщиков.
+    :param weights: весовые коэффициенты каждого элемента входного вектора.
+    :return: предсказание.
+
+    """
+
+    prediction = inp.dot(weights)
+    return prediction
+
+
+def main():
+    """Основная функция"""
+
+    toes = np.array([8.5, 9.5, 9.9, 9.0])
+    wl_rec = np.array([0.65, 0.8, 0.8, 0.9])
+    num_fans = np.array([1.2, 1.3, 0.5, 1.0])
+
+    inp = np.array([toes[0], wl_rec[0], num_fans[0]])
+    prediction = neural_network(inp, WEIGHTS)
+    print(prediction)
+
+
+if __name__ == "__main__":
+    main()
+
